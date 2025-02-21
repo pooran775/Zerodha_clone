1309fc52e00b1b93cb45f3e77ae93049e3cf6b64
pooran775 <pooran07072005@gmail.com>
2025-02-22 03:31:59 +0530

    final changes

--- dashboard/src/components/Orders.js
+++ dashboard/src/components/Orders.js
@@ -1,17 +1,76 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import axios, { all } from "axios";
+import { VerticalGraph } from "./VerticalGraph";
+
+// import { holdings } from "../data/data";
 
 const Orders = () => {
-  return (
-    <div className="orders">
-      <div className="no-orders">
-        <p>You haven't placed any orders today</p>
+  const [allOrders, setAllOrders] = useState([]);
 
-        <Link to={"/"} className="btn">
-          Get started
-        </Link>
+  useEffect(() => {
+    axios.get("http://localhost:3002/allOrders").then((res) => {
+      // console.log(res.data);
+      setAllOrders(res.data);
+    });
+  }, []);
+
+  // const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  const labels = allOrders.map((subArray) => subArray["name"]);
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Stock Price",
+        data: allOrders.map((stock) => stock.price),
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+    ],
+  };
+
+  // export const data = {
+  //   labels,
+  //   datasets: [
+  // {
+  //   label: 'Dataset 1',
+  //   data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+  //   backgroundColor: 'rgba(255, 99, 132, 0.5)',
+  // },
+  //     {
+  //       label: 'Dataset 2',
+  //       data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+  //       backgroundColor: 'rgba(53, 162, 235, 0.5)',
+  //     },
+  //   ],
+  // };
+
+  return (
+    <>
+      <h3 className="title">Orders ({allOrders.length})</h3>
+
+      <div className="order-table">
+        <table>
+          <tr>
+            <th>Nane</th>
+            <th>Qty</th>
+            <th>Price</th>
+            <th>Mode</th>
+          </tr>
+
+          {allOrders.map((stock, index) => {
+            return (
+              <tr key={index}>
+                <td>{stock.name}</td>
+                <td>{stock.qty}</td>
+                <td>{stock.price}</td>
+                <td>{stock.mode}</td>
+              </tr>
+            );
+          })}
+        </table>
       </div>
-    </div>
+      <VerticalGraph data={data} />
+    </>
   );
 };
 
